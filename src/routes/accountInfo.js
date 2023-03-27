7bc9f5110bb484074feaa2088e35afb4f791d3c2
chrscbs <johnchristiancabang@gmail.com>
2023-03-27 21:33:27 +0800

    fix: printing null upon reading the json file

--- src/routes/accountInfo.js
+++ src/routes/accountInfo.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 /* GET account information endpoint */
 router.get('/', (req, res) => {
     // throw new Error('This is my created error')
-    fs.readFile('src/localDBs/accountInfo.json', 'utf-8', (jsonString) => {
+    fs.readFile('src/localDBs/accountInfo.json', 'utf-8', (err, jsonString) => {
         const data = JSON.parse(jsonString);
         console.log(data)
         res.json(data)
