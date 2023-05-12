92cd99ad92bc228b74b52a375450429383ffd0c4
chrscbs <johnchristiancabang@gmail.com>
2023-05-12 12:48:33 +0800

    feat: save streamId to local file store

--- src/routes/deviceLinkRequest.js
+++ src/routes/deviceLinkRequest.js
@@ -11,12 +11,31 @@ const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 router.use(express.json())
 
 // TODO: create a function for getting the device streamId
-function generate_streamId() {
-    const streamId = "AM_R3B2D_00_ENZ, AM_R3B2D_00_ENN" // mock values
-    return streamId
-
+async function generate_streamId() {
     // TODO: Get streamID from rshake
     // TODO: Parse streamId and save details to deviceInfo.json
+    let retVal = ""
+    const streamId = "AM_R3B2D_00_ENZ,AM_R3B2D_00_ENN" // mock values
+
+    try {
+        const deviceInfo = {
+            deviceInfo: {
+                network: streamId,
+                station: streamId,
+                location: streamId,
+                elevation: streamId,
+                channel: streamId,
+                streamId: streamId
+            }
+        };
+        await fs.promises.writeFile("src/localDBs/deviceInfo.json", JSON.stringify(deviceInfo));
+
+        retVal = streamId
+    } catch (error) {
+        retVal = { status: 400, message: "StreamID not acquired" }
+    }
+
+    return retVal
 }
 
 // Request token from auth server + save token to localDB 
@@ -39,8 +58,11 @@ async function request_auth_token(username, password) {
                 password: password,
                 role: data.role
             };
-            const response = await axios.post(auth_url, credentials, { httpsAgent });
-            console.dir("request_auth_token response: " + response, {depth: null});
+            const response = (process.env.NODE_ENV === 'production')
+                ? await client.post(auth_url, credentials)
+                : await axios.post(auth_url, credentials)
+            console.log("request_auth_token response: " + response.data.accessToken);
+            console.dir(response)
             const jsonToken = {
                 accessToken: response.accessToken,
                 role: data.role
@@ -72,10 +94,11 @@ router.post('/',
             next()
         }
     }, async (req, res, next) => {
-        const streamId = generate_streamId(); //get device streamId
-        console.log("streamId Acquired: " + streamId) //logs the streamId acquired
-        const macAddress = getmac.default(); //get device mac address
-        console.log('Device Mac Address Acquired: ' + macAddress); //logs the Mac Address acquired
+        try {
+            const streamId = await generate_streamId(); //get device streamId
+            console.log("streamId Acquired: " + streamId) //logs the streamId acquired
+            const macAddress = getmac.default(); //get device mac address
+            console.log('Device Mac Address Acquired: ' + macAddress); //logs the Mac Address acquired
 
         const token = await request_auth_token(req.body.username, req.body.password);
         console.log("Returned value: " + token)
@@ -94,30 +117,41 @@ router.post('/',
             ? 'https://' + process.env.W1_PROD_HOST + '/device/link'
             : 'http://' + process.env.W1_DEV_HOST + ':' + process.env.W1_DEV_PORT + '/device/link';
 
-        axios.post(url, json)
-            .then(response => {
-                // console.log(response)
-                res.status(response.status).json({
-                    status: response.status,
-                    message: 'Succesfully Request Linking to W1'
+            const response = (process.env.NODE_ENV === 'production')
+                ? await client.post(url, json, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                    rejectUnauthorized: false // disable SSL verification
                 })
-            })
-            .catch(error => {
-                // console.log(error)
-                if (error.response) {
-                    // The request was made and the server responded with a status code that falls out of the range of 2xx
-                    res.status(error.response.status).json({
-                        status: error.response.status,
-                        message: error.response.data.message
-                    })
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    res.status(500).json({
-                        status: 500,
-                        message: "Internal Server Error"//error.response.data.message
-                    })
-                }
-            })
+                : await axios.post(url, json, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+            res.status(response.status).json({
+                status: response.status,
+                message: 'Successfully Requested Linking to W1'
+            });
+        } catch (error) {
+            // console.log(error);
+
+            if (error.response) {
+                // The request was made and the server responded with a status code that falls out of the range of 2xx
+                res.status(error.response.status).json({
+                    status: error.response.status,
+                    message: "Error from earthquake-hub: " + error.response.data.message
+                });
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                res.status(500).json({
+                    status: 500,
+                    message: 'Internal Server Error'
+                });
+            }
+        }
+
     })
 
 
