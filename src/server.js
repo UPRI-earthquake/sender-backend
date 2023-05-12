6c01c6cb200da6236d272d31d727cbf175c74fce
chrscbs <johnchristiancabang@gmail.com>
2023-05-12 07:02:05 -0400

    feat: incorporate .env vars for backend ip

--- src/server.js
+++ src/server.js
@@ -5,7 +5,11 @@ require('dotenv').config()
 const port = process.env.NODE_ENV === 'production'
              ? process.env.BACKEND_PROD_PORT
              : process.env.BACKEND_DEV_PORT;
+
+const ip = process.env.NODE_ENV === 'production'
+             ? process.env.BACKEND_PROD_IP
+             : process.env.BACKEND_DEV_IP;
              
-app.listen(port, () => {
+app.listen(port,ip, () => {
     console.log('App listening on port: ' + port)
 })
