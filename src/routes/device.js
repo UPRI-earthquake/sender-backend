a1bf96927c81145c9e9027093407b400e0ae457b
chrscbs <johnchristiancabang@gmail.com>
2023-05-24 17:17:36 +0800

    chore: remove unused imports

--- src/routes/device.js
+++ src/routes/device.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const path = require('path');
 const { spawn } = require('child_process');
-
-router.use(bodyParser.json())
 
 // POST endpoint (/stream) to execute a slink2dali childprocess from nodejs
 router.route('/stream').post( async (req, res) => {
