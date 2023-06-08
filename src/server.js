ee9b71b5949d6665bf11504dbd9d18e6efcf0688
chrscbs <johnchristiancabang@gmail.com>
2023-06-09 02:50:56 +0800

    feat: initialize dictionary and start streaming on node server start

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 // import app from './app'
 const app = require('./app')
+const { initializeStreamsObject, spawnSlink2dali } = require('./controllers/stream.controller')
 require('dotenv').config()
 
+// Asynchronous function for initializing streamsObject dictionary, then start streaming on each supplied url.
+async function init() {
+  try {
+    let streamsObject = await initializeStreamsObject();
+
+    for (const url in streamsObject) {
+      if (streamsObject.hasOwnProperty(url)) {
+        await spawnSlink2dali(url);
+      }
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+}
+
+// Call the main function
+init();
+
+
 const port = process.env.NODE_ENV === 'production'
-             ? process.env.BACKEND_PROD_PORT
-             : process.env.BACKEND_DEV_PORT;
+  ? process.env.BACKEND_PROD_PORT
+  : process.env.BACKEND_DEV_PORT;
 
 const ip = process.env.NODE_ENV === 'production'
-             ? process.env.BACKEND_PROD_IP
-             : process.env.BACKEND_DEV_IP;
-             
-app.listen(port,ip, () => {
-    console.log('App listening on port: ' + port)
+  ? process.env.BACKEND_PROD_IP
+  : process.env.BACKEND_DEV_IP;
+
+app.listen(port, ip, () => {
+  console.log('App listening on port: ' + port)
 })
