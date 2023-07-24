9f1796337f2423e8d3d1cb38473cdd036bcc6328
chrscbs <johnchristiancabang@gmail.com>
2023-07-24 16:09:55 +0800

    chore: add swagger ui documentation to /deviceLinkRequest endpoint

--- src/routes/deviceLinkRequest.js
+++ src/routes/deviceLinkRequest.js
@@ -11,6 +11,40 @@ const { generate_streamId } = require('./utils');
 
 router.use(express.json())
 
+
+/**
+ * @swagger
+ * /deviceLinkRequest:
+ *   post:
+ *     summary: Endpoint for linking the device to a registered account in earthquake-hub network (this endpoint is dependent to earthquake-hub-backend, meaning to test this endpoint make sure that ehub-backend container is up)
+ *     tags:
+ *       - Device
+ *     requestBody:
+ *       description: User credentials registered in earthquake-hub network
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Username registered in earthquake-hub network
+ *               password:
+ *                 type: string
+ *                 description: Account's password
+ *             example:
+ *               username: citizen
+ *               password: testpassword
+ *     responses:
+ *       200:
+ *         description: Successful response with device information
+ *       400:
+ *         description: Device already linked to an existing account
+ *       500:
+ *         description: Internal server error
+ */
+
 // Request token from auth server + save token to localDB
 // note: Always check if there's an existing token in localDB, if none, request from auth server
 async function request_auth_token(username, password) {
