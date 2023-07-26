3a98c42f0a840030594635b99e133320a495e5f6
chrscbs <johnchristiancabang@gmail.com>
2023-07-26 23:47:51 +0800

    chore: refactor /stream/start, /stream/status endpoints to follow route-controller-service structure

---
+++ src/controllers/device.controller.js
@@ -0,0 +1,61 @@
+const fs = require('fs').promises;
+const streamController = require('./stream.controller')
+
+
+let streamsObject = {};
+
+// A function that returns streamsObject
+async function populateStreamsObject() {
+  try {
+    streamsObject = await streamController.getStreamsObject();
+    return streamsObject;
+  } catch (error) {
+    console.error(`Error getting streamsObject Dictionary: ${error}`);
+  }
+}
+
+// Middleware function that checks if the device is already linked to an account; Status should not be 'Streaming'. Should not proceed if 'Not yet linked'.
+async function streamStatusCheck(req, res, next) {
+  await streamController.getStreamsObject();
+
+  const url = req.body.url;
+  if (!streamsObject.hasOwnProperty(url)) {
+    return res.status(409).json({ message: 'Ringserver URL is invalid' })
+  }
+
+  if (streamsObject[url].status === 'Streaming') {
+    return res.status(409).json({ message: `Device is already streaming to ${url}` })
+  }
+
+  next(); // Proceed to the next middleware/route handler
+}
+
+async function startStreaming(req, res) {
+  console.log('POST Request sent on /stream/start endpoint')
+
+  try {
+    await spawnSlink2dali(req.body.url);
+    res.status(200).json({ message: 'Child Process Spawned Successfully' });
+  } catch (error) {
+    console.log(`Error spawning slink2dali: ${error}`)
+    res.status(500).json({ error: 'Error spawning child process' });
+  }
+}
+
+async function getStreamingStatus(req, res) {
+  console.log('GET Request sent on /stream/status endpoint')
+  streamsObject = await streamController.getStreamsObject();
+
+  const outputObject = {};
+
+  for (const url in streamsObject) {
+    if (streamsObject.hasOwnProperty(url)) {
+      const { status, hostName, retryCount } = streamsObject[url];
+      outputObject[url] = { status, hostName, retryCount };
+    }
+  }
+
+  res.status(200).json({ message: 'Get Streams Status Success', payload: outputObject })
+}
+
+module.exports = { getStreamingStatus, streamStatusCheck, startStreaming };
--- src/routes/device.js
+++ src/routes/device.js
@@ -4,9 +4,9 @@ const fs = require('fs')
 const path = require('path')
 const bodyParser = require('body-parser')
 const { getStreamsObject, spawnSlink2dali } = require('../controllers/stream.controller');
+const deviceController = require('../controllers/device.controller')
 
 router.use(bodyParser.json())
-
 
 /**
  * @swagger
@@ -21,48 +21,10 @@ router.use(bodyParser.json())
  *       500:
  *         description: Internal server error
  */
-let streamsObject = {};
-
-// A function that returns streamsObject
-async function populateStreamsObject() {
-  try {
-    streamsObject = await getStreamsObject();
-    return streamsObject;
-  } catch (error) {
-    console.error(`Error getting streamsObject Dictionary: ${error}`);
-  }
-}
-
-// Middleware function to check the streaming status to the specified server url; status should not be 'Streaming'
-async function streamStatusCheck(req, res, next) {
-  const url = req.body.url;
-  if (!streamsObject.hasOwnProperty(url)) {
-    return res.status(409).json({ message: 'Ringserver URL is invalid' })
-  }
-
-  if (streamsObject[url].status === 'Streaming') {
-    return res.status(409).json({ message: `Device is already streaming to ${url}` })
-  }
-
-  next(); // Proceed to the next middleware/route handler
-}
-
-// TODO: Add middleware function that checks if the device is already linked to an account; should not proceed if not yet linked.
-router.route('/stream/start').post(async (req, res, next) => {
-  await populateStreamsObject();
-  next();
-}, streamStatusCheck,
-  async (req, res) => {
-    console.log('POST Request sent on /stream/start endpoint')
-
-    try {
-      await spawnSlink2dali(req.body.url);
-      res.status(200).json({ message: 'Child Process Spawned Successfully' });
-    } catch (error) {
-      console.log(`Error spawning slink2dali: ${error}`)
-      res.status(500).json({ error: 'Error spawning child process' });
-    }
-  })
+router.post('/stream/start', 
+  deviceController.streamStatusCheck, //Middleware function that checks if the device is already linked to an account; should not proceed if not yet linked.
+  deviceController.startStreaming
+)
 
 /**
  * @swagger
@@ -77,20 +39,6 @@ router.route('/stream/start').post(async (req, res, next) => {
  *       500:
  *         description: Internal server error
  */
-router.route('/stream/status').get(async (req, res) => {
-  console.log('GET Request sent on /stream/status endpoint')
-  streamsObject = await populateStreamsObject();
-
-  const outputObject = {};
-
-  for (const url in streamsObject) {
-    if (streamsObject.hasOwnProperty(url)) {
-      const { status, hostName, retryCount } = streamsObject[url];
-      outputObject[url] = { status, hostName, retryCount };
-    }
-  }
-
-  res.status(200).json({ message: 'Get Streams Status Success', payload: outputObject })
-})
+router.get('/stream/status', deviceController.getStreamingStatus)
 
 module.exports = router;
