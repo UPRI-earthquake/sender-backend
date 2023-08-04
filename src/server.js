6323cd605c3ff30933d90032a491296896872f01
chrscbs <johnchristiancabang@gmail.com>
2023-08-04 16:21:50 +0800

    feat: cors client expected ips

--- src/server.js
+++ src/server.js
@@ -36,5 +36,8 @@ const ip = process.env.NODE_ENV === 'production'
   : process.env.BACKEND_DEV_IP;
 
 app.listen(port, ip, () => {
-  console.log('App listening on port: ' + port)
+  console.log(`App listening on: ${ip}:${port}`);
+  process.env.NODE_ENV === 'production'
+   ? console.log(`Production client expected (by CORS) at: http://${process.env.CLIENT_PROD_IP}:${process.env.CLIENT_PROD_PORT}`)
+   : console.log(`Development client expected (by CORS) at: http://${process.env.CLIENT_DEV_IP}:${process.env.CLIENT_DEV_PORT}`)
 })
